e61c86d8a4d78f61eadd753978567bda1a72c38f
realqdbp <me@qdbp.codes>
2022-07-31 22:46:43 +0200

    Bugfix: Fixed crafting command

--- src/main/java/codes/qdbp/serverplugin/commands/CraftCommand.java
+++ src/main/java/codes/qdbp/serverplugin/commands/CraftCommand.java
@@ -1,11 +1,9 @@
 package codes.qdbp.serverplugin.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryType;
 import org.jetbrains.annotations.NotNull;
 
 public class CraftCommand implements CommandExecutor {
@@ -15,7 +13,7 @@ public class CraftCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            player.openInventory(Bukkit.createInventory(player, InventoryType.WORKBENCH));
+            player.openWorkbench(player.getLocation(), true);
         }
         return true;
     }
